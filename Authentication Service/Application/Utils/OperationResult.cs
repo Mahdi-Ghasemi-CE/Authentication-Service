a43d749bbb6f6ef7a8cfc47a03ee27a0f793a492
Mahdi-Ghasemi-CE <mahdighasemi.shahed@gmail.com>
2024-04-15 14:39:34 +0330

    Add - IService base interface for services

---
+++ Authentication Service/Application/Utils/OperationResult.cs
@@ -0,0 +1,28 @@
+namespace Authentication_Service.Application.Utils;
+using System.Net;
+
+public class OperationResult
+{
+    public readonly HttpStatusCode Status;
+    public readonly object Value;
+
+    public OperationResult(HttpStatusCode status, object value)
+    {
+        Status = status;
+        Value = value;
+    }
+
+    public bool Succeeded => IsSucceeded(Status);
+
+    private bool IsSucceeded(HttpStatusCode status) => status switch
+    {
+        _ when
+            status == HttpStatusCode.OK => true,
+        _ when
+            status == HttpStatusCode.NotAcceptable ||
+            status == HttpStatusCode.NotFound => false,
+        _ => false
+    };
+
+    public OperationResult Clone() => (OperationResult)MemberwiseClone();
+}
